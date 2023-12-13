4e9010a6084daff1dda5565d9f0a5ac37c7c7816
Umaru Emmanuel Isaac <117543583+Realyyt@users.noreply.github.com>
2023-12-13 16:47:27 +0000

    chore: proflie update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">UMARU EMMANUEL ISAAC </span>
-            from <span className="purple"> Nigeria.</span>
+            <br />
             I am currently employed as a software developer at
             Paycrest
             <br />
